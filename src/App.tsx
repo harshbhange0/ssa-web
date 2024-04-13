50d5eae85cb8994859f302b2e68990a9e7f30bdd
harshbhane0 <harshbhange123@gmail.com>
2024-04-13 22:13:54 +0530

    Remove import of 'useNavigate' from react-router-dom in App.tsx
    Update Select.tsx to remove unnecessary import of React.
    
    These changes do not affect the functionality of the code, but rather clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import SignComponent from "./pages/auth";
 import { Slide, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
