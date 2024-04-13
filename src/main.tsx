02626062796e433c7738407296974728cad2cb52
harshbhane0 <harshbhange123@gmail.com>
2024-04-13 16:16:28 +0530

    Add Vercel Analytics to project
    Adds @vercel/analytics package and imports Analytics component in main.tsx.
    
    Also updates package-lock.json with new package and dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import RecoilContextProvider from "./provider/RecoilProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <RecoilContextProvider>
       <App />
+      <Analytics />
     </RecoilContextProvider>
   </BrowserRouter>,
 );
